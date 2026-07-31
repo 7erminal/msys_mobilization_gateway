ce8f5f90301a3a7bca0767371a15ab75bb83fe35
Terminal-Adawe <bede.abbe91@gmail.com>
2026-07-31 13:26:27 +0000

    changes

--- controllers/clients.go
+++ controllers/clients.go
@@ -85,7 +85,7 @@ func (c *ClientsController) GetAll() {
 	var sortby []string
 	var order []string
 	var query = make(map[string]string)
-	var limit int64 = 10
+	var limit int64 = 500
 	var offset int64
 
 	// fields: col1,col2,entity.col3
