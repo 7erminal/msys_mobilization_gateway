33f6dec73b0373cfc63eb9a2a56d35df29e6270c
Terminal-Adawe <bede.abbe91@gmail.com>
2026-08-12 09:51:59 +0000

    changes

--- controllers/functions/hostMapping.go
+++ controllers/functions/hostMapping.go
@@ -66,6 +66,9 @@ func HostMapping(clientid string) (host string) {
 	if clientid == "889001" {
 		tHost, _ = beego.AppConfig.String("pakyimanCoopApiBaseUrl")
 	}
+	if clientid == "889002" {
+		tHost, _ = beego.AppConfig.String("asomdwieCCUApiBaseUrl")
+	}
 
 	return tHost
 }
